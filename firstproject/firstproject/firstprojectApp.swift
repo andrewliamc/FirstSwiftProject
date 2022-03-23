864360fc8f5200dc51791d2bb5227f2fd6341c85
Andrew Liam C <andrewliamc@startmail.com>
2022-03-23 16:18:11 -0400

    Setup
    initializing the project and creating the starter files

---
+++ firstproject/firstproject/firstprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstprojectApp.swift
+//  firstproject
+//
+//  Created by andrew . on 3/23/22.
+//
+
+import SwiftUI
+
+@main
+struct firstprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
